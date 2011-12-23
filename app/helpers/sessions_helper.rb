32eb557361645211183d145d5aaac69960a99584
Googol Lee <googollee@gmail.com>
2011-12-23 17:24:07 +0800

    switch to https with sign related url

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,7 +31,7 @@ module SessionsHelper
 
   def deny_access
     store_location
-    redirect_to signin_path, :notice => "Please sign in to access this page."
+    redirect_to signin_url(:protocol => 'https'), :notice => "Please sign in to access this page."
   end
 
   def redirect_back_or(default)
--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,20 @@ Yaif::Application.routes.draw do
   match '/crontab', :to => 'triggers#show_crontab', :format => :text
   match '/trigger', :to => 'triggers#trigger', :format => :text
 
-  match '/signup', :to => 'users#new'
-  match '/signin', :to => 'sessions#new'
-  match '/signout', :to => 'sessions#destroy'
+  scope :constraints => { :protocol => 'http' } do
+    match '/signup', :to => redirect { |params, request|
+                                        put params
+                                        "https://" + request.host_with_port + request.fullpath
+    }
+    match '/signin', :to => redirect { |params, request|
+                                        put params
+                                        "https://" + request.host_with_port + request.fullpath
+    }
+    match '/signout', :to => redirect { |params, request|
+                                        put params
+                                        "https://" + request.host_with_port + request.fullpath
+    }
+  end
 
   root :to => 'sessions#root'
 end
